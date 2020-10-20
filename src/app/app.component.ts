bef0585e203b3a29f34b79c0e344d14e44f8156c
Dean Alam <s1gamingc@gmail.com>
2020-10-20 03:05:28 -0400

    Procfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tip-calc';
+  title = "Dean's Tip Calculator";
+  price;
+  tax;
+  total;
+  low;
+  med;
+  high;
+  finalTotalLow;
+  finalTotalMed;
+  finalTotalHigh;
+
+  calculateTotal(price, tax){
+    this.total = price + (price * (tax / 100));
+    this.low = (price * 0.1);
+    this.med = (price * 0.15);
+    this.high = (price * 0.2);
+    this.finalTotalLow = this.total + this.low;
+    this.finalTotalMed = this.total + this.med;
+    this.finalTotalHigh = this.total + this.high;
+  }
 }
